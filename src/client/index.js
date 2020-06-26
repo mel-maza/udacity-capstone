1cb67b4528f5d1c1c52793749c788a8ae0e9934b
Melanie <melanie_foeldes@gmx.de>
2020-06-26 13:38:21 +0200

    fetch location data from geonames

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,33 @@
+import './styles/style.scss';
+
 alert("New project");
+
+// Main Function
+const getTravelInfo = (city) => {
+    // const city = document.getElementById('city').value;
+    const data = {
+        city
+    };
+    postData('http://localhost:3000/travelInfo', data)
+        .then((response) => console.log(response));
+}
+
+// Fetch Functions
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const locationData = await response.json();
+        return locationData;
+    } catch (error) {
+        console.log('An error occurred while updating the location info: ', error);
+    }
+}
+
+getTravelInfo('Wilhermsdorf');
--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const fetch = require('node-fetch');
 
 dotenv.config();
 
@@ -20,3 +21,22 @@ app.listen(3000, () => {
 
 // Routes
 app.get('/', (req, res) => {res.sendFile('dist/index.html');})
+
+app.post('/travelInfo', (req, res) => {
+    // Get location info from Geonames
+    const city = req.body.city;
+    getLocationInfoForCity(process.env.GEO_URL + city + '&maxRows=1&username=' + process.env.GEO_USERNAME)
+        .then(response => {console.log(response); res.send(response)});
+})
+
+// Fetch Functions
+const getLocationInfoForCity = async (url = '') => {
+    const response = await fetch(url);
+    try {
+        const locationData = await response.json();
+        return locationData;
+    } catch (error) {
+        console.log('An error occurred while calling the GEOnames-API: ', error);
+        throw error;
+    }
+}
